d0215eb4b5986b384cf9430c74231b133cd04db7
jacksoncoldiron <jcoldiron@ucsb.edu>
2025-02-27 08:16:44 -0800

    Added my name to the comment

--- R/autopower.R
+++ R/autopower.R
@@ -13,6 +13,7 @@
 #' @return power (W)
 
 #autopower function
+# Jackson Coldiron
 autopower = function(V, m, A, cdrag=0.3, crolling=0.015,pair=1.2,g=9.8) {
    P = crolling*m*g*V + 1/2*A*pair*cdrag*V**3
   return(P)
